2535a644ba27d9ff2af1ea3594d8b2cbc43c226a
Ann Enders <annenders@gmail.com>
2013-10-07 19:26:48 -0500

    yay design work is pretty

--- public/js/app.js
+++ public/js/app.js
@@ -6,5 +6,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.ejs', controller: MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.ejs', controller: MyCtrl2});
+    $routeProvider.when('/view3', {templateUrl: 'partials/partial3.ejs', controller: MyCtrl3});
+    $routeProvider.when('/view4', {templateUrl: 'partials/partial4.ejs', controller: MyCtrl4});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,3 +10,11 @@ MyCtrl1.$inject = [];
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+function MyCtrl3() {
+}
+MyCtrl3.$inject = [];
+
+function MyCtrl4() {
+}
+MyCtrl4.$inject = [];
